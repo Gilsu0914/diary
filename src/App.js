d53009ca01d10864f56b18a032b94973b0df7da0
Gilsu0914 <gilsu0126@gmail.com>
2022-10-11 12:49:15 +0900

    Add:normalizeCss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import {BrowserRouter, Routes, Route,} from 'react-router-dom';
+import {BrowserRouter, Routes, Route,} from 'react-router-dom'
 
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import Signup from './pages/signup/Signup';
+import Nav from './components/Nav'
+
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav/>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
